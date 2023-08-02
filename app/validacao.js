937d8d14b1aca7e6a3433850cdb2685c8f71c7e9
Lui Mendes <127629779+Luiprogramador@users.noreply.github.com>
2023-08-02 11:16:54 -0300

    Modifiquei a tela de game Over!

--- app/validacao.js
+++ app/validacao.js
@@ -7,7 +7,7 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             document.body.innerHTML =
                 `
                 <h2>Game Over!!!</h2>
-                <h3>Pressione o botão para jogar novamente</h3>
+                <h3>O número secreto era ${numeroSecreto}</h3>
                 <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
                 `
                 document.body.style.backgroundColor = "black";
